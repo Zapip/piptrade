925b445f92624bd91fe14fe386cf1a80c29a3e83
Zapip <zafif3165@gmail.com>
2024-11-18 14:05:35 +0700

    initial commit vue 3

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    <h1 class="text-3xl bg-red-900">tailwind test</h1>
+  </main>
+</template>
